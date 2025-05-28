79c4459d47b6b1905baadbfab42fb845f4ef7cea
MorozovSergey81 <srmorozov@edu.hse.ru>
2025-05-29 00:45:32 +0500

    Update build.sh with better logging

--- harmony_habit_tracker/build.sh
+++ harmony_habit_tracker/build.sh
@@ -2,6 +2,19 @@
 # exit on error
 set -o errexit
 
+echo "Current directory:"
+pwd
+echo "Directory contents:"
+ls -la
+
+echo "Python version:"
+python --version
+
+echo "Upgrading pip..."
 python -m pip install --upgrade pip
+
+echo "Installing requirements..."
 pip install -r requirements.txt
 
+echo "Installed packages:"
+pip list
